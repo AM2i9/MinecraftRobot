5004004720ef9a547290efc583f41e9bdf92a8a6
Patrick Brennan <brennanpatrick20@gmail.com>
2022-05-06 15:21:14 -0400

    Adding MecanumDrive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,25 +7,14 @@ package frc.robot;
 import java.time.Duration;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import io.graversen.minecraft.rcon.MinecraftRcon;
-import io.graversen.minecraft.rcon.commands.SayCommand;
 import io.graversen.minecraft.rcon.service.ConnectOptions;
 import io.graversen.minecraft.rcon.service.MinecraftRconService;
 import io.graversen.minecraft.rcon.service.RconDetails;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
   private final MinecraftRconService rconService = new MinecraftRconService(
     RconDetails.localhost("PASSWORD"),
@@ -33,85 +22,55 @@ public class Robot extends TimedRobot {
   );
   private MinecraftRcon rcon;
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private PWMTalonSRX frontLeft;
+  private PWMTalonSRX frontRight;
+  private PWMTalonSRX backLeft;
+  private PWMTalonSRX backRight;
+
+  private MecanumDrive drive;
+
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
 
+    frontLeft = new PWMTalonSRX(0);
+    frontRight = new PWMTalonSRX(2);
+    backLeft = new PWMTalonSRX(1);
+    backRight = new PWMTalonSRX(3);
+
+    drive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
+
+    // Create Minecraft RCON connection
+    //! currently throws error if server isn't running
     rconService.connectBlocking(Duration.ofSeconds(10));
     rcon = rconService.minecraftRcon().orElseThrow(IllegalStateException::new);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {}
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select between different
-   * autonomous modes using the dashboard. The sendable chooser code works with the Java
-   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-   * uncomment the getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to the switch structure
-   * below with additional strings. If using the SendableChooser make sure to add them to the
-   * chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
-
-  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     String resp = rcon.sendSync(new GetLeverStateCommand(0)).getResponseString();
     System.out.println("Lever state " + resp.substring(10, 11));
   }
 
-  /** This function is called once when the robot is disabled. */
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
   @Override
   public void disabledInit() {}
 
-  /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {}
 
-  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {}
-
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
